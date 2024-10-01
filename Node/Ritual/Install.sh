3ed1c015a4f598a5594525b67b202bee9cd1a934
crazydimka <dima.rabecki@gmail.com>
2024-10-02 01:01:28 +0300

    Update Install.sh

--- Node/Ritual/Install.sh
+++ Node/Ritual/Install.sh
@@ -106,7 +106,7 @@ sleep 3
 sudo rm -rf docker-compose.yaml
 wget https://raw.githubusercontent.com/blackcat-team/kuznica/refs/heads/main/Node/Ritual/docker-compose.yaml
 
-docker-compose up --remove-orphans -d
+docker compose up --remove-orphans -d
 
 docker rm -fv infernet-anvil  &>/dev/null
 
